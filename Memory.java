ef3b15b8231d2669218651fbe8de67e1178c066f
bw-leran <brandonmillerut@gmail.com>
2022-04-18 09:09:13 -1000

    add files

---
+++ Memory.java
@@ -0,0 +1,17 @@
+public class Memory {
+
+    int[] cell;
+    int cap = 256;
+
+    public static Integer read(int addr) {
+        return addr;
+    }
+
+    public static Integer write(int addr, int data) {
+        return data;
+    }
+
+    public static String dump() {
+        return "dump here";
+    }
+}
