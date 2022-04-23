bd47bcf241e2379858399568dc1f3a378952edb7
DeclanJones01 <djones42@my.hpu.edu>
2022-04-22 17:24:07 -1000

    Added contents to Memory and Processor
    I wasn't able to load program1.m1

--- Memory.java
+++ Memory.java
@@ -1,20 +1,38 @@
 public class Memory {
 
-    int[] cell;
-    int cap = 256;
+    private int[] cell;
+    private int cap;
 
     public Memory(int cap) {
+        this.cap = cap;
+        cell = new int[cap];
     }
 
-    public Integer read(int addr) {
-        return addr;
+    public Memory() {
+        this(256);
     }
 
-    public Integer write(int addr, int data) {
-        return data;
+    public int read(int addr) {
+        if (addr < 0 || cap <= addr) {
+            System.out.println("bad address: " + addr);
+            System.exit(1);
+        }
+        return cell[addr];
     }
 
-    public String dump() {
-        return "dump here";
+    public void write(int addr, int data) {
+        if (addr < 0 || cap <= addr) {
+            System.out.println("bad address: " + addr);
+            System.exit(1);
+        }
+        cell[addr] = data;
     }
+
+    public void dump() {
+        System.out.println("Memory:");
+        for (int i = 0; i < cap; i++) {
+            System.out.println("cell[" + Integer.toHexString(i) + "] = " + Integer.toHexString(cell[i]));
+        }
+    }
+
 }
--- Processor.java
+++ Processor.java
@@ -1,21 +1,104 @@
 public class Processor {
 
-    int PC;
-    int IR;
-    int[] reg;
+    private int numRegs = 8;
+    private int[] reg = new int[numRegs];
+    private int PC, IR;
+    private Memory memory;
 
-    public Boolean step(){
-        return true;
+    public void setMemory(Memory m) {
+        memory = m;
     }
 
-    public String dump(){
-        return "dump here";
+    public void setPC(int addr) {
+        PC = addr;
     }
 
-    public void setMemory(Memory memory) {
+    public boolean step() {
+        IR = memory.read(PC++);
+        if (IR == 0) {
+            return true;
+        }
+        int opcode = IR >> 8;
+        int arg1 = (IR >> 4) & 0xf;
+        int arg2 = IR & 0xf;
+        switch (opcode) {
+            case 1: // load
+                reg[arg1] = memory.read(reg[arg2]);
+                break;
+            case 2: // loadc
+                reg[arg1] = memory.read(PC++);
+                break;
+            case 3: // store
+                memory.write(reg[arg1], reg[arg2]);
+                break;
+            case 4: // add
+                reg[arg1] = reg[arg1] + reg[arg2];
+                break;
+            case 5:  // mul
+                reg[arg1] = reg[arg1] * reg[arg2];
+                break;
+            case 6:  // sub
+                reg[arg1] = reg[arg1] - reg[arg2];
+                break;
+            case 7:  // div
+                if (reg[arg2] == 0) {
+                    System.out.println("divide by 0 error!");
+                    System.exit(1);
+                }
+                reg[arg1] = reg[arg1] / reg[arg2];
+                break;
+            case 8: // and
+                if (reg[arg1] != 0 && reg[arg2] != 0) {
+                    reg[arg1] = 1;
+                } else {
+                    reg[arg1] = 0;
+                }
+                break;
+            case 9:  // or
+                if (reg[arg1] != 0 || reg[arg2] != 0) {
+                    reg[arg1] = 1;
+                } else {
+                    reg[arg1] = 0;
+                }
+                break;
+            case 10: // not
+                if (reg[arg2] != 0) {
+                    reg[arg1] = 0;
+                } else {
+                    reg[arg1] = 1;
+                }
+                break;
+            case 11:  // lshift
+                reg[arg1] = reg[arg2] << 1;
+                break;
+            case 12:  // rshift
+                reg[arg1] = reg[arg2] >> 1;
+                break;
+            case 13:  // bwc
+                reg[arg1] = reg[arg1] & reg[arg2];
+                break;
+            case 14:   // bwd
+                reg[arg1] = reg[arg1] | reg[arg2];
+                break;
+            case 15:
+                if (reg[arg1] != 0) {
+                    PC = reg[arg2];
+                }
+                break;
+            default:
+                System.out.println("unrecognized opcode: " + opcode);
+                System.exit(1);
+        }
+        return false;
     }
 
-    public void setPC(int i) {
-        int PC = i;
+    public void dump() {
+        System.out.println("Registers:");
+        for (int i = 0; i < numRegs; i++) {
+            System.out.println("reg[" + Integer.toHexString(i) + "] = " + Integer.toHexString(reg[i]));
+        }
+        System.out.println("PC = " + Integer.toHexString(PC));
+        System.out.println("IR = " + Integer.toHexString(IR));
     }
+
 }
