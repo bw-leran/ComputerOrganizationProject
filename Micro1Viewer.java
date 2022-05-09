5d52cffe16f33441d7d57cb0aeaa4416bba35615
bw-leran <70430003+bw-leran@users.noreply.github.com>
2022-05-08 14:52:25 -1000

    add button actionlisteners

--- Micro1Viewer.java
+++ Micro1Viewer.java
@@ -1,5 +1,8 @@
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
 import java.nio.BufferOverflowException;
+import java.awt.event.ActionEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,31 +21,60 @@ public class Micro1Viewer {
         frame.setVisible(true);
         frame.add(panel);
 
-        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
+        panel.setLayout(null);
 
         JLabel title = new JLabel("Micro-1 Viewer (Anastasia, Brandon, and Declan)");
+        title.setBounds(350, 0, 1000, 100);
         panel.add(title);
 
         JButton load_button = new JButton("Load");
+        load_button.setBounds(100, 150, 100, 40);
+        load_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.print("pressed");
+            }
+        });
         panel.add(load_button);
 
         // this one should dump to command console
         JButton memory_button = new JButton("Memory");
+        memory_button.setBounds(300, 150, 100, 40);
+        memory_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.print("pressed");
+            }
+        });
         panel.add(memory_button);
 
         // this one should dump to jtextfield
         JButton registers_button = new JButton("Registers");
+        registers_button.setBounds(500, 150, 100, 40);
+        registers_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.print("pressed");
+            }
+        });
         panel.add(registers_button);
 
         // produce success or error message to jtextfield
         JButton step_button = new JButton("Step");
+        step_button.setBounds(700, 150, 100, 40);
+        step_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.print("pressed");
+            }
+        });
         panel.add(step_button);
-
+        
         JTextField registers_textfield = new JTextField("registers go here!");
+        registers_textfield.setBounds(100, 300, 700, 400);
         panel.add(registers_textfield);
+        // currently cant do multiple lines
 
-        JTextField step_textfield = new JTextField("step success or fail message go here!");
-        panel.add(step_textfield);
 
     }
 
