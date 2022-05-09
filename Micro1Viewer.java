54dda755c6641d52cae32f1dab92fa3a959cefbb
bw-leran <70430003+bw-leran@users.noreply.github.com>
2022-05-08 14:05:38 -1000

    add panel

--- Micro1Viewer.java
+++ Micro1Viewer.java
@@ -1,5 +1,16 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class Micro1Viewer {
     
-    
+    public static void main(String[] args) {
+
+        JPanel panel = new JPanel();
+        JFrame frame = new JFrame();
+        frame.setSize(1000, 1000);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.add(panel);
+    }
 
 }
